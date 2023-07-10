c98278e118d85bb7a62272b75cf23e3ce9a65093
Sean O'Connor <so@seano.dev>
2023-07-10 11:46:42 -0600

    Initial code commit

---
+++ Ada/AdaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdaApp.swift
+//  Ada
+//
+//  Created by Sean O'Connor on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AdaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ada/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ada
+//
+//  Created by Sean O'Connor on 7/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
