f9f33e46ff42c56a8e6bf32d15a80e68ffb4c2a1
Sean O'Connor <so@seano.dev>
2023-07-17 16:29:33 -0600

    Add preview models

--- Ada/API/Models/User.swift
+++ Ada/API/Models/User.swift
@@ -19,4 +19,6 @@ extension User: Equatable {
     static func == (lhs: User, rhs: User) -> Bool {
         lhs.id == rhs.id
     }
+
+    static let Preview = User(id: "pg", created: 1160418092, karma: 157044, about: "Bug fixer.", submitted: [35172415, 34043420, 34042993, 22654365, 22654342, 21505741, 21256727])
 }
